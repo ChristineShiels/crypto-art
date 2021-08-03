d0a1fb1597925bc1a4e2f3e44c87d2fc13460700
christineshiels <christineshiels@gmail.com>
2021-08-03 07:25:33 -0700

    front page w stylez :art: :partly_sunny:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class classComponentName extends Component {
+
+
+  render() {
+      return (
+          <main className="wrapper">
+            <div className="front-page">
+              <div className="fp-main-content">
+                
+                  <h1 className="main-title">A Picture Worth a Thousand Blocks</h1>
+                  <h2 className="main-subtitle">Decrypting the Relationship Between Carbon, Cryptocurrencies, and NFTs</h2>
+                  <h3 className="byline">words and visuals by Mark Belan and Adam Zucker | <a href="https://www.google.com">About the artists ></a></h3>
+                <div className="divider"></div>
+                <p className="main-copy">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus molestiae cumque aut, maxime voluptatem aperiam rerum quasi repellat minima ipsa inventore sunt eveniet nemo. Molestias aliquam suscipit praesentium, consequatur minus illo tempore? Autem aspernatur nobis est dolore amet suscipit iusto beatae veritatis voluptatum optio, in cumque cupiditate facilis quasi vero, architecto facere velit expedita accusamus? Non atque ipsa rerum consequuntur molestias numquam. Quas consequatur molestias asperiores consequuntur quidem laudantium sapiente autem incidunt quisquam nostrum officia illo nesciunt ut, voluptatem totam ipsum minima dolorem, voluptas molestiae labore. Amet quia magni ex beatae sequi eos aut dolorem laudantium iste. Delectus eos consequuntur officia blanditiis aperiam? Sunt?</p>
+              </div>
+              <div className="fp-side-content">
+                <div className="clip-path clip-path-border">
+                  <div className="clip-path clip-path-front">
+                    <div className="side-link">
+                      <a href="#" class="main-nav-link"><span class="bold">Click</span> to explore</a>
+                      
+                    </div>
+                    <svg width="0" height="0">
+                      <clipPath transform="scale(0.00052083, 0.00092593)" id="fp-curve" clipPathUnits="objectBoundingBox">
+                        <path d="M1923.74.6v1080H3.74a2056,2056,0,0,0,436.5-174.5c139.91-75.58,234.41-147.8,341-230,353.24-272.44,330.92-347.34,566-475,186.12-101.08,390-157.23,415-164C1827.19,19.54,1882.8,8.06,1923.74.6Z"></path>
+                      </clipPath>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </main>
+      )
+  }
 }
 
-export default App;
+export default classComponentName;
